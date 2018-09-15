5fe5b7ed738afca6542640b3cb80c09109b1d2c1
tharangni <tharangni@hotmail.com>
2018-09-15 17:43:43 +0200

    Update multilabel.py

--- evaluate/multilabel.py
+++ evaluate/multilabel.py
@@ -7,4 +7,6 @@ class Multilabel:
     @staticmethod
     def f1_scores(y_true, y_pred):
         # Compute F1 score per class and return as a dictionary
+        # why micro? : https://nlp.stanford.edu/IR-book/html/htmledition/evaluation-of-text-classification-1.html
+        # "Microaveraged results are a measure of effectiveness on the large classes in a test collection"
         return f1_score(y_true, y_pred, average="micro") # why micro?
