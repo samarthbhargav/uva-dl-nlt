74897306548f409741dd0370aff73f3219a5d5c2
tharangni <tharangni@hotmail.com>
2018-09-17 01:16:17 +0200

    doc2vec v1 + lda dataloader check(TODO)

--- models/lda.py
+++ models/lda.py
@@ -2,6 +2,8 @@ import os
 import pathlib
 
 import spacy
+
+import numpy as np
 from gensim import models
 from gensim.test.utils import datapath
 
@@ -13,28 +15,44 @@ class LdaModel:
         self.num_topics = num_topics
         self.lda = None
         self.vocabulary = vocabulary
-        self.modelPath = os.path.join(os.getcwd(), "checkpoints", "lda")
+
+        # self.modelPath = os.path.join(os.getcwd(), "checkpoints", "lda")
+        temp = os.path.join(os.getcwd(), "checkpoints", "lda")
+        if not os.path.exists(temp):
+            os.makedirs("checkpoints/lda")
+        self.modelPath = temp
+
         self.modelName = "lda-model"
-    
+
     def fit(self, data):
         pathlib.Path(self.modelPath).mkdir(parents=True, exist_ok=True)
         modelFile = datapath(os.path.join(self.modelPath, self.modelName))
+
         try:
             lda = models.LdaModel.load(modelFile)
         except FileNotFoundError:
             lda = models.LdaModel(self.doc2bow(data), num_topics=100, minimum_probability=0)
             lda.save(modelFile)
+
         self.lda = lda
-    
+
     def predict(self, texts):
         return [list(zip(*sorted(self.lda[text], key=lambda _: -_[1]))) for text in self.doc2bow(texts)]
 
     def doc2bow(self, data):
         processedData = []
+        wordList = []
         record = data
-        wordList = [int(word) for word in record[2]]
+
+        # wordList = [int(word) for word in record[2]]
+        for _, line in enumerate(record):
+            for tensor in line[2]:
+                wordList.append(int(np.array(tensor)))
+
         newRecord = []
-        for word in record[2]:
+
+        # for word in record[2]:
+        for word in wordList:
             newRecord.append((int(word), wordList.count(int(word))))
         processedData.append(newRecord)
         return processedData
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
+import numpy as np
 import logging as log
 
-import numpy as np
 import torch
 from torch import nn
 from torch import optim
@@ -9,14 +9,17 @@ from torch.utils.data import DataLoader
 
 from data_utils import file_utils
 from args_utils import get_argparser
-from data_utils.vocabulary import Vocabulary
-from models.deep_models import SimpleDeepModel
-from models.lda import LdaModel as LDA
-from models.random_forest import RandomForestModel as RandomForest
-from data_utils.dataloader import ReutersDataset, ReutersDatasetIterator
-from evaluate.multilabel import Multilabel
-from evaluate import eval_utils
 
+from data_utils.vocabulary import Vocabulary
+from data_utils.dataloader import ReutersDataset, ReutersDatasetIterator
+
+from models.lda import LdaModel as LDA
+from models.doc2vec import doc2vecModel as Doc2Vec
+from models.deep_models import SimpleDeepModel
+from models.random_forest import RandomForestModel as RandomForest
+
+from evaluate import eval_utils
+from evaluate.multilabel import Multilabel
 
 if __name__ == '__main__':
     args = get_argparser().parse_args()
@@ -35,6 +38,8 @@ if __name__ == '__main__':
             vocabulary = file_utils.load_obj(vocab_path)
         else:
             log.info("Vocab doesn't exist. Creating")
+            if not os.path.exists("common_persist"):
+                os.makedirs("common_persist")
             vocabulary = Vocabulary(
                 remove_stopwords, min_freq, lowercase, "./data/reuters/stopwords")
             vocabulary.build(train_iter)
@@ -46,7 +51,30 @@ if __name__ == '__main__':
         train_loader = DataLoader(train_set, shuffle=True, batch_size=1)
         test_loader = DataLoader(test_set, shuffle=False, batch_size=1)
 
-        if args.model == "lda":
+        if args.model == "doc2vec":
+            doc2vec = Doc2Vec(num_words=100, min_count=2, epochs=40)
+            train_corpus = list(doc2vec.tagging(corpus=train_loader))
+            test_corpus = list(doc2vec.tagging(corpus=test_loader, testing = True))
+
+            # sanity-check
+            print(len(train_corpus))
+            print(len(test_corpus))
+
+            doc2vec.train(train_corpus=train_corpus)
+
+            # to reduce memory usage after training
+            doc2vec.model.delete_temporary_training_data(keep_doctags_vectors=True, keep_inference=True)
+
+            doc_id, sims = doc2vec.test(test_corpus=test_corpus)
+            # Compare and print the most/median/least similar documents from the train corpus
+            print('Test Document ({}): «{}»\n'.format(doc_id, ' '.join(test_corpus[doc_id])))
+            print(u'SIMILAR/DISSIMILAR DOCS PER MODEL %s:\n' % doc2vec.model)
+            for label, index in [('MOST', 0), ('MEDIAN', len(sims)//2), ('LEAST', len(sims) - 1)]:
+                print(u'%s %s: «%s»\n' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))
+
+
+
+        elif args.model == "lda":
             lda = LDA(num_topics=100, vocabulary=vocabulary)
             lda.fit(train_loader)
 
@@ -76,6 +104,7 @@ if __name__ == '__main__':
                 Multilabel.f1_scores(groundtruth, predictions)))
 
         elif args.model == "simple-deep":
+
             model = SimpleDeepModel(len(train_set.label_dict), len(vocabulary))
 
             optimizer = optim.Adam(model.parameters())
@@ -102,4 +131,3 @@ if __name__ == '__main__':
                     Multilabel.f1_scores(y_true, y_pred)))
     else:
         raise ValueError("Unknown module")
-
