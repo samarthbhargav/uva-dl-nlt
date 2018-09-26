6e38ad3ab16434affd68a96b023b2c975eaaa98a
tharangni <tharangni@hotmail.com>
2018-09-26 19:48:34 +0200

    doc2vec eval completed. f1 score is low

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ import numpy as np
 import logging as log
 
 import torch
-import numpy as np
 from torch import nn
 from torch import optim
 from torch.utils.data import DataLoader
@@ -14,9 +13,10 @@ from args_utils import get_argparser
 from data_utils.vocabulary import Vocabulary
 from data_utils.dataloader import ReutersDataset, ReutersDatasetIterator
 
-from models.lda import TrainLdaModel
+from models.lda import LdaModel as LDA
 from models.doc2vec import doc2vecModel as Doc2Vec
-from models.deep_models import SimpleDeepModel, Sequence2Multilabel
+from models.deep_models import SimpleDeepModel
+from models.random_forest import RandomForestModel as RandomForest
 from models.embedding_models import GloVeEmbeddings, EmbeddingCompositionModel
 
 from evaluate import eval_utils
@@ -26,18 +26,13 @@ if __name__ == '__main__':
     args = get_argparser().parse_args()
 
     if args.verbose:
-        log.basicConfig(level=log.DEBUG)
-    else:
         log.basicConfig(level=log.INFO)
+    else:
+        log.basicConfig(level=log.DEBUG)
 
     remove_stopwords = True
     min_freq = 5
     lowercase = True
-
-    # seed everything for reproducability
-    # what is the Ultimate Answer to Life, The Universe and Everything?
-    np.random.seed(42)
-    torch.manual_seed(42)
 
     if args.module == "train":
         train_iter = ReutersDatasetIterator(args.data_root, "training")
@@ -61,57 +56,100 @@ if __name__ == '__main__':
         test_loader = DataLoader(test_set, shuffle=False, batch_size=1)
 
         if args.model == "doc2vec":
-            doc2vec = Doc2Vec(num_words=100, min_count=2, epochs=40)
+            doc2vec_model_path = "common_persist/doc2vec_model5.pkl"
+            train_tagged_path = "common_persist/train_tagged5.pkl"
+            test_tagged_path = "common_persist/test_tagged5.pkl"
 
-            train_corpus = list(doc2vec.tagging(corpus=train_loader))
-            test_corpus = list(doc2vec.tagging(
-                corpus=test_loader, testing=True))
+            if os.path.exists(train_tagged_path and test_tagged_path and doc2vec_model_path):
+                print("Loading existing model and tagged corpus...")
+                doc2vec = file_utils.load_obj(doc2vec_model_path)
+                train_corpus = file_utils.load_obj(train_tagged_path)
+                test_corpus = file_utils.load_obj(test_tagged_path)
+                print("Doc2vec models loaded!")
 
-            # sanity-check
-            print(len(train_corpus))
-            print(len(test_corpus))
+            else:
+                print("Doc2vec model doesn't exist. Creating it...")
+                doc2vec = Doc2Vec(num_words=100, min_count=2, epochs=40)
+                train_corpus = doc2vec.tagging(corpus=train_loader)
+                test_corpus = doc2vec.tagging(corpus=test_loader, testing=True)
+                file_utils.save_obj(doc2vec, doc2vec_model_path)
+                file_utils.save_obj(train_corpus, train_tagged_path)
+                file_utils.save_obj(test_corpus, test_tagged_path)
+                print("Doc2vec models created!")
 
-            doc2vec.train(train_corpus=train_corpus)
+            doc2vec.train_doc2vec(train_corpus=train_corpus)
+            clf = doc2vec.build_train_classifier(corpus=train_corpus)
+            doc2vec.build_test_classifier(test_corpus=test_corpus, train_corpus=train_corpus, clf = clf)
 
-            # to reduce memory usage after training
-            doc2vec.model.delete_temporary_training_data(
-                keep_doctags_vectors=True, keep_inference=True)
-
-            doc_id, sims = doc2vec.test(test_corpus=test_corpus)
-
-            # Compare and print the most/median/least similar documents from the train corpus
-            print('Test Document ({}): «{}»\n'.format(
-                doc_id, ' '.join(test_corpus[doc_id])))
-            print(u'SIMILAR/DISSIMILAR DOCS PER MODEL %s:\n' % doc2vec.model)
-            for label, index in [('MOST', 0), ('MEDIAN', len(sims)//2), ('LEAST', len(sims) - 1)]:
-                print(u'%s %s: «%s»\n' % (label, sims[index], ' '.join(
-                    train_corpus[sims[index][0]].words)))
 
         elif args.model == "lda":
-            ldaModel = TrainLdaModel(args.num_topics, vocabulary)
-            ldaModel.train(train_loader)
-            ldaModel.eval(test_loader)
+            lda = LDA(num_topics=100, vocabulary=vocabulary)
+            lda.fit(train_loader)
+
+            X = []
+            y = []
+            for index, train_datapoint in enumerate(train_loader):
+                X.append(lda.predict(train_datapoint)[0][0])
+                y.append(list(train_datapoint[1][0].numpy()))
+                if (index + 1) % 100 == 0:
+                    print("Predicting LDA {}/{}".format(index + 1, len(train_loader)))
+
+            randomForest = RandomForest()
+            randomForest.fit([X, y])
+
+            groundtruth = []
+            predictions = []
+            for index, test_datapoint in enumerate(test_loader):
+                prediction = randomForest.predict(
+                    [lda.predict(test_datapoint)[0][0]])
+                predictions.extend(prediction.tolist())
+                groundtruth.append(list(test_datapoint[1][0].numpy()))
+                if (index + 1) % 100 == 0:
+                    print(
+                        "Predicting Random Forest {}/{}".format(index + 1, len(test_loader)))
+
+            groundtruth, predictions = np.array(
+                groundtruth), np.array(predictions)
+
+            print("Test F1: {}".format(
+                Multilabel.f1_scores(groundtruth, predictions)))
 
         elif args.model == "simple-deep":
-            assert args.epochs > 0, "Provide number of epochs"
-            seq2ml = Sequence2Multilabel(len(train_set.label_dict), len(
-                vocabulary.vocab), torch.cuda.is_available())
-            seq2ml.train(train_loader, test_loader, args.epochs)
+            model = SimpleDeepModel(len(train_set.label_dict), len(vocabulary))
+
+            optimizer = optim.Adam(model.parameters())
+            criterion = nn.BCEWithLogitsLoss()
+            epochs = 10
+            # y_true, y_pred = eval_utils.gather_outputs(test_set, model)
+            # log.info("Test F1: {}".format(
+            #     Multilabel.f1_scores(y_true, y_pred)))
+            for epoch in range(epochs):
+                for _id, labels, text, _,  _, _ in train_loader:
+                    labels = torch.FloatTensor(labels)
+                    model.zero_grad()
+                    model.hidden = model.init_hidden()
+                    seq = torch.LongTensor(text)
+                    output = model.forward(seq)
+                    loss = criterion(output, labels)
+                    loss.backward()
+                    optimizer.step()
+
+                y_true, y_pred = eval_utils.gather_outputs(test_set, model)
+                log.info("Test F1: {}".format(
+                    Multilabel.f1_scores(y_true, y_pred)))
+
         elif args.model == "embedding-glove":
-            assert args.composition_method is not None, "Provide composition method"
-            assert args.epochs > 0, "Provide number of epochs"
+            assert args.composition_method is not None, "Please provide composition method"
             glove_model_path = "./common_persist/glove.pkl"
             if os.path.exists(glove_model_path):
                 log.info("Loading existing glove model")
                 glove = file_utils.load_obj(glove_model_path)
             else:
                 log.info("Reading and saving glove model")
-                glove = GloVeEmbeddings(
-                    "./common_persist/embeddings/glove.6B.300d.txt", vocabulary)
+                glove = GloVeEmbeddings("./common_persist/embeddings/glove.6B.300d.txt", vocabulary)
                 file_utils.save_obj(glove, glove_model_path)
-            embedding_model = EmbeddingCompositionModel(
-                glove, args.composition_method)
-            embedding_model.fit(train_loader, test_loader, args.epochs)
+            embedding_model = EmbeddingCompositionModel(glove, args.composition_method)
+            embedding_model.fit(train_loader, test_loader, 30)
         else:
             raise ValueError("Unknown model: {}".format(args.model))
 
