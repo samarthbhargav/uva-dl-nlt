12ee5a6e0b76ad4ae3a32f0ad72b8e19b6d13a3d
Masoumeh Bakhtiariziabari <31740498+mbakhtiariz@users.noreply.github.com>
2018-09-28 09:27:00 +0200

    f1 test = 0.84, f1 train = 0.99 for 100 epochs

--- models/HAN.py
+++ models/HAN.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
+import torch.optim.lr_scheduler
 import math
 
 
@@ -12,21 +13,22 @@ import logging as log
 import torch.optim as optim
 from evaluate.multilabel import Multilabel
 
+
 class WordAttention(nn.Module):
-    def __init__(self, batch_size, embedding_size):
+    def __init__(self, batch_size, embedding_size, GRU_dropout):
 
         super(WordAttention, self).__init__()
 
         self.embedding_size = embedding_size
         self.GRU_hid_size = 10
-        self.GRU_dropout = 0.0
+        self.GRU_dropout = GRU_dropout
         self.word_att_size = self.GRU_hid_size * 2
         self.batch_size = batch_size
-
+        self.gru_num_layers = 1
 
         # Word Encoder
         #self.embedding = nn.Embedding.from_pretrained(pretrained_weights)
-        self.word_gru = nn.GRU(input_size=self.embedding_size, hidden_size=self.GRU_hid_size, num_layers=1,
+        self.word_gru = nn.GRU(input_size=self.embedding_size, hidden_size=self.GRU_hid_size, num_layers=self.gru_num_layers,
                                bias=True, batch_first=True, dropout=self.GRU_dropout, bidirectional=True)
         #Word Attention
         self.word_att_lin = nn.Linear(self.GRU_hid_size * 2, self.word_att_size)
@@ -61,19 +63,20 @@ class WordAttention(nn.Module):
 
 
 class SentAttention(nn.Module):
-    def __init__(self, batch_size, embedding_size, cls_num):
+    def __init__(self, batch_size, embedding_size, cls_num, GRU_dropout):
 
         super(SentAttention, self).__init__()
 
         self.embedding_size = embedding_size
         self.GRU_hid_size = 10
-        self.GRU_dropout = 0.0
+        self.GRU_dropout = GRU_dropout
         self.word_att_size = self.GRU_hid_size * 2
         self.cls_num = cls_num
         self.batch_size = batch_size
+        self.gru_num_layers = 1
 
         #sentence Encoder
-        self.sent_gru = nn.GRU(input_size=self.word_att_size, hidden_size=self.GRU_hid_size, num_layers=1,
+        self.sent_gru = nn.GRU(input_size=self.word_att_size, hidden_size=self.GRU_hid_size, num_layers=self.gru_num_layers,
                           bias=True, batch_first=True, dropout=self.GRU_dropout, bidirectional=True)
         #sentence Attention
         self.sent_att_lin = nn.Linear(self.GRU_hid_size * 2, self.word_att_size)
@@ -114,12 +117,23 @@ class hanTrainer(object):
 
         self.embeddings = embeddings
         self.batch_size = 64
-        self.max_sent_len = 20
-        self.max_num_sent = 10
+        self.max_sent_len = 20  # Note: if you change it here you have to change it in vocabularyHAN.py as well
+        self.max_num_sent = 10  # same for this... Should refactor this.
         self.embedding_size = 300
-        self.word_attention = WordAttention(batch_size=self.batch_size, embedding_size=self.embedding_size)
-        self.sent_attention = SentAttention(batch_size=self.batch_size, embedding_size=self.embedding_size, cls_num=90)
+        self.learning_rate = 0.005#0.005
+        self.learning_rate_type = "step" #"normal"
+        #self.learning_rate_type = "normal"
+        self.exp_num = 9
+        self.log_file_path = "D:\\courses\\dl4nlt\\results\\"
+        self.neg_weight = 0.4
+        self.GRU_dropout = 0.4
+
+        self.word_attention = WordAttention(batch_size=self.batch_size, embedding_size=self.embedding_size,
+                                            GRU_dropout=self.GRU_dropout)
+        self.sent_attention = SentAttention(batch_size=self.batch_size, embedding_size=self.embedding_size, cls_num=90,
+                                            GRU_dropout=self.GRU_dropout)
         self.cuda = torch.cuda.is_available() #and False
+        self.log_hyperparams()
         log.info("Using CUDA: {}".format(self.cuda))
         #self.device = "cpu"#torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -146,7 +160,7 @@ class hanTrainer(object):
             if len(batch) == batch_size:
                 for b in range(batch_size):
                     for s in range(10):
-                        if len(batch[b][s]) != 20:
+                        if len(batch[b][s]) != self.max_sent_len:
                             print ("len was bad at", b, s, len(batch[b][s]))
                 batch = np.array(batch).astype(float)
                 labels_batch = np.array(labels_batch, dtype=float)
@@ -173,13 +187,12 @@ class hanTrainer(object):
             for text_batch, labels_batch in self._batch(loader, self.batch_size):
                 if self.cuda:
                     text_batch, labels_batch = text_batch.cuda(), labels_batch.cuda()
-                if text_batch.size()[0] != 64:
+                if text_batch.size()[0] != self.batch_size:
                     break
                 predictions = self.forward(text_batch)
                 tloss = self.bce(predictions.view(-1), labels_batch.view(-1), 0.5, 0.5)
-                #!print(tloss.item())
-                probs =  torch.sigmoid(predictions)#F.sigmoid(predictions)
-                output =  torch.sigmoid(predictions)#F.sigmoid(predictions)
+                probs = torch.sigmoid(predictions)#F.sigmoid(predictions)
+                output = torch.sigmoid(predictions)#F.sigmoid(predictions)
                 output[output >= threshold] = 1
                 output[output < threshold] = 0
                 y_pred.extend(output.cpu().numpy())
@@ -193,36 +206,21 @@ class hanTrainer(object):
         s = self.word_attention(text_batch)
         return self.sent_attention(s)
 
-    def my_own_loss(self, predictions, labels_batch):
-        s = 0
-        for i in range(predictions.shape[0]):
-            s += - math.log(max(0.0001, predictions[int(labels_batch[i])]))
-        return s / predictions.shape[0]
-
-    def bce2(self, p, l):
-        eps = 1e-8
-        khar = (1 - l) * np.log(np.clip(1 - p, a_min=eps, a_max=1))
-        gav = l * np.log(np.clip(p, a_min=eps, a_max=1))
-        print("++++++++++++", l)
-        print("+++++++++++++", 1-l)
-        print("---------", khar)
-        print("---------", gav)
-        return - np.mean(gav + khar)
-
     def bce(self, p, l, w_neg, w_pos):
         eps = 1e-8
         p = torch.sigmoid(p)#F.sigmoid(p)
         return - (w_pos * l * torch.log(p.clamp(min=eps)) + w_neg*(1 - l) * torch.log((1 - p).clamp(min=eps))).mean()
 
-
     def fit(self, train_loader, test_loader, epochs):
-
         if self.cuda:
             self.word_attention = self.word_attention.cuda()
             self.sent_attention = self.sent_attention.cuda()
 
-        word_optimizer = optim.Adam(self.word_attention.parameters(), lr=0.005)
-        sent_optimizer = optim.Adam(self.sent_attention.parameters(), lr=0.005)
+        word_optimizer = optim.Adam(self.word_attention.parameters(), lr=self.learning_rate)
+        sent_optimizer = optim.Adam(self.sent_attention.parameters(), lr=self.learning_rate)
+        scheduler_word = torch.optim.lr_scheduler.StepLR(word_optimizer, step_size=40, gamma=0.2)
+        scheduler_sent = torch.optim.lr_scheduler.StepLR(sent_optimizer, step_size=40, gamma=0.2)
+        best_fscore = 0
 
         for epoch in range(epochs):
             log.info("Epoch: {}".format(epoch))
@@ -230,6 +228,7 @@ class hanTrainer(object):
             self.sent_attention.train(True)
 
             count = 0
+            all_loss = []
             for text_batch, labels_batch in self._batch(train_loader, self.batch_size):
                 '''
                 count += 1
@@ -246,20 +245,54 @@ class hanTrainer(object):
                 self.word_attention.zero_grad()
                 predictions = self.forward(text_batch)
 
-                loss = self.bce(predictions.view(-1), labels_batch.view(-1), 0.02, 0.98)
-
+                loss = self.bce(predictions.view(-1), labels_batch.view(-1), self.neg_weight, 1 - self.neg_weight)
 
                 loss.backward()
                 sent_optimizer.step()
                 word_optimizer.step()
 
-                #!log.info("Loss: {}".format(loss.item()))
+                all_loss.append(loss.item())
 
+                count += 1
+                if count % 500 == 0:
+                    log.info("Loss: {}".format(loss.item()))
+                    break
+            train_loss = np.mean(all_loss)
+            log.info("Train avg Loss: {}".format(train_loss))
+            self.log_score_to_file(os.path.join(self.log_file_path, "{}_loss_train.txt".format(self.exp_num)),
+                                   train_loss)
             y_true, y_pred, y_prob = self.gather_outputs(test_loader)
-            log.info("Test F1: {}".format(
-                Multilabel.f1_scores(y_true, y_pred)))
-            #print(y_prob[0], y_prob[5], y_prob[10])
-
+            test_f_score = Multilabel.f1_scores(y_true, y_pred)
+            log.info("Test F1: {}".format(test_f_score))
+            self.log_score_to_file(os.path.join(self.log_file_path, "{}_f_score_test.txt".format(self.exp_num)),
+                                   test_f_score)
+            if self.learning_rate_type == "step":
+                scheduler_sent.step()
+                scheduler_word.step()
             y_true, y_pred, y_prob = self.gather_outputs(train_loader)
-            log.info("Train F1: {}".format(
-                Multilabel.f1_scores(y_true, y_pred)))
+            train_f_score = Multilabel.f1_scores(y_true, y_pred)
+            log.info("Train F1: {}".format(train_f_score))
+            self.log_score_to_file(os.path.join(self.log_file_path, "{}_f_score_train.txt".format(self.exp_num)),
+                                   train_f_score)
+            if test_f_score >= best_fscore:
+                best_fscore = test_f_score
+                torch.save(self.sent_attention.state_dict(),
+                           os.path.join(self.log_file_path, '{}_best_model_sent.pt').format(self.exp_num))
+                torch.save(self.word_attention.state_dict(),
+                           os.path.join(self.log_file_path, '{}_best_model_word.pt').format(self.exp_num))
+
+    def log_hyperparams(self):
+        f = open(os.path.join(self.log_file_path, "{}_hyperparams.txt".format(self.exp_num)), "a")
+        f.write("lr_type:{}\nlr_val:{}\n".format(self.learning_rate_type, self.learning_rate))
+        f.write("neg_w:{}\n".format(self.neg_weight))
+        f.write("batch_size:{}\n".format(self.batch_size))
+        f.write("max_sent_len:{}\n".format(self.max_sent_len))
+        f.write("max_num_sent:{}\n".format(self.max_num_sent))
+        f.write("gru_dropout:{}\n".format(self.GRU_dropout))
+        f.write("------------------------------\n")
+        f.close()
+
+    def log_score_to_file(self, path, score):
+        f = open(path, "a")
+        f.write("{}\n".format(score))
+        f.close()
