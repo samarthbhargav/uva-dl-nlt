b3ac226dc3d261d4908138db8046c42366e925a4
samarthbhargav <samarth.bhargav92@gmail.com>
2018-10-02 14:46:40 +0200

    Fix model

--- run_embedding_glove.sh
+++ run_embedding_glove.sh
@@ -3,11 +3,11 @@ PREFIX="srun --gres=gpu:1 -p fatq python run.py train --data-root $DATA_ROOT "
 
 ## Embedding models
 NUM_EPOCHS=30
-CMD="$PREFIX --model-id embedding_glove_avg --epochs $NUM_EPOCHS --composition-method avg"
+CMD="$PREFIX --model-id embedding_glove_avg --model embedding-glove --epochs $NUM_EPOCHS --composition-method avg"
 echo $CMD; eval $CMD
-CMD="$PREFIX --model-id embedding_glove_sum --epochs $NUM_EPOCHS --composition-method sum"
+CMD="$PREFIX --model-id embedding_glove_sum --model embedding-glove --epochs $NUM_EPOCHS --composition-method sum"
 echo $CMD; eval $CMD
-CMD="$PREFIX --model-id embedding_glove_min --epochs $NUM_EPOCHS --composition-method min"
+CMD="$PREFIX --model-id embedding_glove_min --model embedding-glove --epochs $NUM_EPOCHS --composition-method min"
 echo $CMD; eval $CMD
-CMD="$PREFIX --model-id embedding_glove_max --epochs $NUM_EPOCHS --composition-method max"
+CMD="$PREFIX --model-id embedding_glove_max --model embedding-glove --epochs $NUM_EPOCHS --composition-method max"
 echo $CMD; eval $CMD
